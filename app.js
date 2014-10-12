4248272f382d68ce3412734effd40e8836ab3302
Michael Garate <mpgarate@gmail.com>
2014-10-12 07:22:37 -0400

    favicon

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ app.get('/', function(req, res) {
 app.use('/js', express.static(__dirname + '/client/js'));
 app.use('/css', express.static(__dirname + '/client/css'));
 app.use('/img', express.static(__dirname + '/client/img'));
+app.use('/', express.static(__dirname + '/client'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.get('/api/destinations', controllers.destinations.list);
